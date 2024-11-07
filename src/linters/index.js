19286cf8d34752ad63a5ed2ef72b3e688b138995
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2024-11-07 14:10:59 +0100

    feat: added ts js linters

--- src/linters/index.js
+++ src/linters/index.js
@@ -1,9 +1,12 @@
 const Clippy = require("./clippy");
 const RustFmt = require("./rustfmt");
+const TSC = require("./tsc");
+const Prettier = require("./prettier");
 
 const linters = {
-	// Linters
 	clippy: Clippy,
 	rustfmt: RustFmt,
+	tsc: TSC,
+	prettier: Prettier,
 };
 module.exports = linters;
