1dacef84b078bd57caec87ac0011b2a4c0c07f52
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-06-01 18:22:23 +0200

    feat: added forgejo support

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { apiCommit, createCheck } from './github/api';
 import { getContext } from './github/context';
 import linters from './linters';
 import { type LintResult, getSummary } from './utils/lint-result';
-import { Octokit } from '@octokit/core';
+import { getOctokit } from '@actions/github';
 
 interface Check {
   lintCheckName: string;
@@ -34,10 +34,10 @@ const runAction = async (): Promise<void> => {
   const gitName = core.getInput('git_user_name');
   const gitEmail = core.getInput('git_user_email');
   const signCommits = core.getInput('git_sign_commits') === 'true';
+  const forgejo = core.getInput('forgejo') === 'true';
+  const api_url = core.getInput('api_url');
 
-  const octokit = new Octokit({
-    auth: context.token
-  });
+  const octokit = getOctokit(context.token);
 
   // If on a PR from fork: Display messages regarding action limitations
   if (context.eventName === 'pull_request' && context.repository.hasFork) {
@@ -162,17 +162,41 @@ const runAction = async (): Promise<void> => {
   core.startGroup('Create check runs with commit annotations');
   let groupClosed = false;
   try {
+    const [owner, repo] = context.repository.repoName.split('/');
+
     await Promise.all(
-      checks.map(async ({ lintCheckName, lintResult, summary }) =>
-        createCheck(
-          lintCheckName,
-          headSha,
-          context,
-          lintResult,
-          neutralCheckOnWarning,
-          summary
-        )
-      )
+      checks.map(async ({ lintCheckName, lintResult, summary }) => {
+        if (forgejo) {
+          return octokit.rest.repos
+            .createCommitStatus({
+              context: lintCheckName,
+              description: `${lintCheckName} found ${summary}`,
+              owner,
+              repo,
+              sha: headSha,
+              state:
+                lintResult.isSuccess ||
+                (lintResult.warning.length === 0 &&
+                  lintResult.error.length === 0) ||
+                (neutralCheckOnWarning &&
+                  lintResult.warning.length > 0 &&
+                  lintResult.error.length === 0)
+                  ? 'success'
+                  : 'failure',
+              target_url: api_url
+            })
+            .then(() => {});
+        } else {
+          return createCheck(
+            lintCheckName,
+            headSha,
+            context,
+            lintResult,
+            neutralCheckOnWarning,
+            summary
+          );
+        }
+      })
     );
   } catch {
     core.endGroup();
